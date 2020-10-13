633d1857cfe997647e95eed0aee9d9625d20cb86
Aldana Vasquez <A309689@santandertecnologia.com.ar>
2020-10-12 21:48:43 -0300

    addPorductos

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const axios = require('axios').default;
 
 app.get('/',  (req, res) => {
   axios.get('http://localhost:3000/users').then((result)=>{
-    res.send(result.data);
+
+// Tu codigo
+
+// Fin de tu Codigo
+
+    res.send(newUserList)
   });
 });
+
+
 
 app.listen(port, () => {
   console.log(port);
